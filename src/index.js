31f6ab9a292b763160d9fea01cd4e59fa2aa0693
Your Name <you@example.com>
2017-08-17 15:05:13 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search-bar';<
-const API_KEY = 'AIzaSyBS6pi-zlMHZV-UOfhAph2t46BUihnbEik';
+import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyCEkyJIC9R3lrePv9kyHVWJXOvZW_RfeNk';
 
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>;
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<App/>, document.querySelector('.container'))
+ReactDOM.render(<App/>, document.querySelector('.container'));
